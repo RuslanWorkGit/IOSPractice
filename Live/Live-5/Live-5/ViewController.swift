84d542c884cde5e0f959a3f4a6ec3ed69b449d4a
RuslanWorkGit <lulka758@gmail.com>
2023-10-12 11:23:11 +0300

    Add some button and UIView to storyboard

--- Live/Live-5/Live-5/ViewController.swift
+++ Live/Live-5/Live-5/ViewController.swift
@@ -11,7 +11,23 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let rect = CGRect(x: 50, y: 100, width: 200, height: 100)
+        let topButton = UIButton(frame: rect)
+        topButton.setTitle("Press me!", for: .normal)
+        topButton.setTitleColor(.blue, for: .normal)
+        topButton.backgroundColor = .yellow
+        view.addSubview(topButton)
+        
+        let rect1 = CGRect(x: 70, y: 150, width: 200, height: 100)
+        let topButton1 = UIButton(frame: rect1)
+        topButton1.setTitle("Press me!", for: .normal)
+        topButton1.setTitleColor(.red, for: .normal)
+        topButton1.backgroundColor = .green
+        view.addSubview(topButton1)
+        
+        view.bringSubviewToFront(topButton)
+        
     }
 
 
