686498b6918e3c9d1bc598429dce3af15ab4b32a
RuslanWorkGit <lulka758@gmail.com>
2023-10-13 16:30:48 +0300

    Done Third task for homework 5

---
+++ Block-5/Homework/Homework-5v3/Homework-5v3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework-5v3
+//
+//  Created by Ruslan Liulka on 13.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
